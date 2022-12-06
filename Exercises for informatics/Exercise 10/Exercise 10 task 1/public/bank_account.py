eeb9126dc85d3ae6e82ea871be58c54d23101458
LAPTOP-2IG6RTBQ\liamk <liam.kontopulos@gmail.com>
2022-12-06 10:15:44 +0100

    commit everything

--- Exercises for informatics/Exercise 10/Exercise 10 task 1/public/bank_account.py
+++ Exercises for informatics/Exercise 10/Exercise 10 task 1/public/bank_account.py
@@ -5,21 +5,42 @@ from currency_converter import convert
 class BankAccount:
 
     def __init__(self, currency="CHF"):
-        counter = 0
+        lst = []
         for g in EXCHANGE_RATES:
-            print(g)
-            print(g == currency)
+            lst.append(g)
+        if currency in lst:
+            self.__currency = currency
+            self.__amount = 0
+        else:
+            raise Warning("Currency not available")
 
     def get_currency(self):
-        pass
+        copy = self.__currency
+        return copy
 
     def get_balance(self):
-        pass
+        copy = self.__amount
+        return copy
 
     def deposit(self, amount, currency="CHF"):
-        pass
+        BankAccount.__init__(currency)
+        if isinstance(amount, float) or isinstance(amount, int):
+            if amount > 0:
+                pass
+            else:
+                raise Warning("can't deposit negative amount")
+        else:
+            raise Warning("deposit must be a number")
+
 
     def withdraw(self, amount, currency="CHF"):
-        pass
+        BankAccount.__init__(currency)
+        if isinstance(amount, float) or isinstance(amount, int):
+            if amount > 0:
+                pass
+            else:
+                raise Warning("can't deposit negative amount")
+        else:
+            raise Warning("deposit must be a number")
 
-BankAccount("USD")
+BankAccount()
