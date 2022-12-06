c81cfd1d44a29d5b71bc640d830680c59f9ec796
LAPTOP-2IG6RTBQ\liamk <liam.kontopulos@gmail.com>
2022-12-06 09:33:12 +0100

    commit everything

--- Exercises for informatics/Exercise 10/Exercise 10 task 1/public/bank_account.py
+++ Exercises for informatics/Exercise 10/Exercise 10 task 1/public/bank_account.py
@@ -1,19 +1,25 @@
-#!/usr/bin/env python3
-# add imports, if necessary
+from exchange_rates import EXCHANGE_RATES
+from currency_converter import convert
+
 
 class BankAccount:
 
     def __init__(self, currency="CHF"):
-        pass
+        counter = 0
+        for g in EXCHANGE_RATES:
+            print(g)
+            print(g == currency)
 
     def get_currency(self):
         pass
 
     def get_balance(self):
         pass
-        
+
     def deposit(self, amount, currency="CHF"):
         pass
-    
+
     def withdraw(self, amount, currency="CHF"):
         pass
+
+BankAccount("USD")
--- Exercises for informatics/Exercise 10/Exercise 10 task 1/public/currency_converter.py
+++ Exercises for informatics/Exercise 10/Exercise 10 task 1/public/currency_converter.py
@@ -1,5 +1,2 @@
-#!/usr/bin/env python3
-# add imports, if necessary
-
 def convert(amount, from_currency, to_currency):
     pass
